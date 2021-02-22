64c07676f1e3e93ac61abb38f02ab63bcbdd8c22
Achyuth Varma <achyuth.varma1@gmail.com>
2021-02-22 13:04:20 +0530

    Changed some CORS policy

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,26 @@ const app = express();
 
 var cors = require("cors");
 
-app.use(cors());
+var allowedOrigins = ["http://localhost:3000", "https://achyuthvarma.com"];
+
+app.use(
+  cors({
+    origin: function (origin: any, callback: any) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
+
 // app.set("trust-proxy", true);
 app.use(json());
 
